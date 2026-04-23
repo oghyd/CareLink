bd0076215cfc91bc2d9d867d0848bdbe3ab5226c
Idriss <idriss.berrissoul@uir.ac.ma>
2026-04-23 01:03:53 +0100

    chore: keep only DBConnection.java in utils/ with package utils

--- src/utils/Validator.java
+++
@@ -1,7 +0,0 @@
-package src.utils;
-
-public class Validator {
-    public static boolean validateEmail(String email) { return false; }
-    public static boolean validateNotEmpty(String chaine) { return false; }
-    public static boolean validateMatricule(String matricule) { return false; }
-}
